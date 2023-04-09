64da668fa29621fbdfe74785c233f947863fd268
rocean <rocean74@gmail.com>
2023-04-10 02:59:41 +0300

    Change csv reading for new format

--- src/main/java/eu/apps4net/MoviesInGenres.java
+++ src/main/java/eu/apps4net/MoviesInGenres.java
@@ -26,44 +26,53 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class MoviesInGenres {
 
     /**
-     * This method uses a regular expression to split each line to a list of strings,
-     * each one representing one column
+     * Βρίσκει τη λέξη στον τίτλο
      *
-     * source: 2ο θέμα, 3ης εργασία ΠΛΗ47, του 2021-2022
-     *
-     * @param line string to be split
+     * @param title
+     * @return int
      */
-    private static String[] processLine(String line) {
-        // Create a regular expression for proper split of each line
-
-        // The regex for characters other than quote (")
-        String otherThanQuote = " [^\"] ";
-
-        // The regex for a quoted string. e.g "whatever1 whatever2"
-        String quotedString = String.format(" \" %s* \" ", otherThanQuote);
-
-        // The regex to split the line using comma (,) but taking into consideration the quoted strings
-        // This means that is a comma is in a quoted string, it should be ignored.
-        String regex = String.format("(?x) " + // enable comments, ignore white spaces
-                        ",                         " + // match a comma
-                        "(?=                       " + // start positive look ahead
-                        "  (?:                     " + //   start non-capturing group 1
-                        "    %s*                   " + //     match 'otherThanQuote' zero or more times
-                        "    %s                    " + //     match 'quotedString'
-                        "  )*                      " + //   end group 1 and repeat it zero or more times
-                        "  %s*                     " + //   match 'otherThanQuote'
-                        "  $                       " + // match the end of the string
-                        ")                         ", // stop positive look ahead
-                otherThanQuote, quotedString, otherThanQuote);
-        String[] tokens = line.split(regex, -1);
-
-        // check for the proper number of columns
-        if (tokens.length == 9) {
-            return tokens;
-        } else {
-            System.err.println("Wrong number of columns for line: " + line);
-            return null;
+    private static int getYearFromTitle(String title) {
+        // Αν η λέξη που βρίσκει δεν είναι αριθμός,
+        // σημαίνει ότι στον τίτλο δεν υπάρχει έτος
+        try {
+            // Παίρνει το substring που βρίσκεται στο τέλος του τίτλου (όπου βρίσκεται το έτος)
+            return Integer.parseInt(title.substring(title.length() - 5, title.length() - 1));
+        } catch (Exception e) {
+            return 0;
         }
+    }
+
+    /**
+     * Διάβασμα μιας γραμμής του CSV και σπάσιμο των δεδομένων που υπάρχουν σ' αυτήν
+     * Τα δεδομένα περνάνε σε αντικείμενο της κλάσης Movie
+     * ,
+     * @param line
+     * @return movie
+     */
+    private static Movie getMovie(String line) {
+        // Παίρνει σε array τα κομμάτια της γραμμής που χωρίζονται με ","
+        String[] lineFields = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+
+        // Διάβασμα του τίτλου και αφαίρεση των εισαγωγικών που υπάρχουν σε κάποιους
+        // από αυτούς
+        String title = lineFields[1].replace("\"", "");
+
+        // Διαβάζει το έτος
+        int year = getYearFromTitle(title);
+
+        // Αφαιρεί το έτος από τον τίτλο, για να μείνει σκέτος αυτός
+        title = title.replace(" (" + String.valueOf(year) + ")", "");
+
+        // Δημιουργία του αντικειμένου movie
+        Movie movie = new Movie(Long.parseLong(lineFields[0]), title, String.valueOf(year));
+
+        // Διάβασμα των κατηγοριών της ταινίας και προσθήκη αυτών
+        // στο αντικείμενο movie
+        for (String genre : lineFields[2].split("[|]")) {
+            movie.addGenre(genre);
+        }
+
+        return movie;
     }
 
     public static class MoviesMapper extends Mapper<Object, Text, Text, IntWritable> {
@@ -76,19 +85,11 @@ public class MoviesInGenres {
             String line = value.toString();
 
             // Αν η γραμμή είναι η επικεφαλίδα του αρχείου, τότε την παραλείπουμε
-            if (line.startsWith("imdbID,")) {
+            if (line.startsWith("movieId,")) {
                 return;
             }
 
-            // Σπάει τη γραμμή σε στοιχεία
-            String[] movieArray = processLine(line);
-
-            // Αν η γραμμή δεν έχει τον αριθμό των στοιχείων που πρέπει, τότε την παραλείπουμε
-            if(movieArray == null) {
-                return;
-            }
-
-            movie = new Movie(movieArray);
+            movie = getMovie(line);
 
             // Αν η ταινία δεν έχει είδη, τότε την παραλείπουμε
             if(movie.getGenres().size() == 0) {
@@ -96,8 +97,8 @@ public class MoviesInGenres {
             }
 
             // Προσθέτει κάθε είδος ταινίας στο context του mapper
-            for(Genre genre : movie.getGenres()) {
-                word.set(genre.getName());
+            for(String genre : movie.getGenres()) {
+                word.set(genre.toLowerCase());
 
                 try {
                     one.set(1);
@@ -143,88 +144,42 @@ public class MoviesInGenres {
      * Αντικείμενο για την αποθήκευση των δεδομένων της ταινίας
      */
     public static class Movie {
-        private final int id;
+        private final long movieId;
         private final String title;
         private final String year;
-        private final String runtime;
-        private final ArrayList<Genre> genres;
-        private final String released;
-        private final String imdbRating;
-        private final String imdbVotes;
-        private final String country;
+        private final ArrayList<String> genres = new ArrayList<>();
 
-        public Movie(String[] movieArray) {
-            this.id = Integer.parseInt(movieArray[0]);
-            this.title = movieArray[1];
-            this.year = movieArray[2];
-            this.runtime = movieArray[3];
-            this.genres = new ArrayList<>();
-
-            // Αφαίρεση των εισαγωγικών από το string
-            movieArray[4] = movieArray[4].replaceAll("\"", "");
-
-            // Σπάει το string σε πίνακα με βάση τον χαρακτήρα ","
-            String[] genresArray = movieArray[4].split(",");
-
-            // Προσθήκη των genres στη λίστα, αφαιρώντας τα πιθανά κενά
-            for (String genre : genresArray) {
-                // Αν το string είναι κενό, τότε παραλείπεται
-                if(genre.equals("")) {
-                    continue;
-                }
-
-                this.genres.add(new Genre(genre.strip()));
-            }
-
-            this.released = movieArray[5];
-            this.imdbRating = movieArray[6];
-            this.imdbVotes = movieArray[7];
-            this.country = movieArray[8];
-        }
-
-        @Override
-        public String toString() {
-            return "Movie{" +
-                    "id=" + id +
-                    ", title='" + title + '\'' +
-                    ", year='" + year + '\'' +
-                    ", runtime='" + runtime + '\'' +
-                    ", genres=" + genres +
-                    ", released='" + released + '\'' +
-                    ", imdbRating='" + imdbRating + '\'' +
-                    ", imdbVotes='" + imdbVotes + '\'' +
-                    ", country='" + country + '\'' +
-                    '}';
+        public Movie(long movieId, String title, String year) {
+            this.movieId = movieId;
+            this.title = title;
+            this.year = year;
         }
 
         public String getTitle() {
             return title;
         }
 
-        public ArrayList<Genre> getGenres() {
+        public ArrayList<String> getGenres() {
             return genres;
         }
-    }
 
-    /**
-     * Κλάση για τα είδη ταινιών
-     */
-    public static class Genre {
-        private final String name;
+        public void addGenre(String genre) {
+            genres.add(genre);
+        }
 
-        public Genre(String name) {
-            this.name = name;
+        public String getYear() {
+            return year;
         }
 
         @Override
         public String toString() {
-            return "Genre{" +
-                    "name='" + name + '\'' +
+            return "Movie{" +
+                    "movieId=" + movieId +
+                    ", title='" + title + '\'' +
+                    ", year='" + year + '\'' +
+                    ", genres=" + genres +
                     '}';
         }
-
-        public String getName() {
-            return name;
-        }
     }
+
 }
